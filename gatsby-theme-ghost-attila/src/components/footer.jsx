9e931e58cfe3619629a903472a24392388612050
Shyam <shyamlohar1996@gmail.com>
2020-05-05 12:08:50 +0530

    updated footer with important links and credits

--- gatsby-theme-ghost-attila/src/components/footer.jsx
+++ gatsby-theme-ghost-attila/src/components/footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql, Link } from "gatsby";
 import rssLogo from "../images/rss.svg";
 
 const Footer = () => {
@@ -19,13 +19,40 @@ const Footer = () => {
             {data.ghostSettings.title} &copy; {new Date().getFullYear()}
           </span>
 
-          <span className="nav-credits">
+          <div className="nav-footer-links">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <a href="/sitemap.xml">Sitemap</a>
+              </li>
+              <li>
+                <a href="/rss">RSS</a>
+              </li>
+              <li>
+                <Link to="/contact">Contact Us</Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* <span className="nav-credits">
             Made with DraftBox &bull;{" "}
             <a href="/rss">
               RSS <img style={{height: '10px'}} src={rssLogo} alt="" />
             </a>
-          </span>
+          </span> */}
         </nav>
+        <hr />
+        <div>
+          <ul className="credits">
+            <li>
+              <a href="https://draftbox.co" target="_blank" rel="norefferer noopener">
+                Made With DraftBox
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
