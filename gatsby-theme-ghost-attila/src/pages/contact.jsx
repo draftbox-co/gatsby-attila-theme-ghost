944fd12135ca236534bcee4b5b15b061e9af0242
Keyur <keyur.raval18@gmail.com>
2020-07-13 15:47:20 +0530

    Adds contact meta support

--- gatsby-theme-ghost-attila/src/pages/contact.jsx
+++ gatsby-theme-ghost-attila/src/pages/contact.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Layout from "../components/Layout";
 import ContactForm from "../components/contact-form";
-
-const Contact = () => {
+import ContactMeta from "./../components/meta/ContactMeta";
+const Contact = ({location}) => {
 
   return (
     <Layout>
+      <ContactMeta location={location}/>
       <ContactForm />
     </Layout>
   );
